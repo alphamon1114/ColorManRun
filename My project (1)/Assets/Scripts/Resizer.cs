660207035c3e11a5f5369de891d2012d9ca5d186
alphamon1114 <alphamon1114@gmail.com>
2026-09-11 17:45:02 +0900

    Area

--- My project (1)/Assets/Scripts/Resizer.cs
+++ My project (1)/Assets/Scripts/Resizer.cs
@@ -1,16 +1,44 @@
+using System.Collections;
 using UnityEngine;
 
 public class Resizer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private Vector3 start;
+    [SerializeField]
+    private Vector3 end;
+    [SerializeField]
+    private float resizeTime = 1f;
+
+    private void OnEnable()
     {
-        
+        StartCoroutine(nameof(Process));
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        StopCoroutine(nameof(Process));
+    }
+
+    IEnumerator Process()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(ScaleTo(start, end));
+
+            yield return StartCoroutine(ScaleTo(end, start));
+        }
+    }
+
+    IEnumerator ScaleTo(Vector3 start, Vector3 end)
+    {
+        float percent = 0;
+        while (percent < 1)
+        {
+            percent += Time.deltaTime / resizeTime;
+            transform.localScale = Vector3.Lerp(start, end, percent);
+            yield return null;
+        }
     }
 }
--- My project (1)/Assets/Scripts/RotateToAxis.cs
+++ My project (1)/Assets/Scripts/RotateToAxis.cs
@@ -1,42 +1,38 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.Rendering;
-using static UnityEngine.Rendering.DebugUI;
 
 public class RotateToAxis : MonoBehaviour
 {
+    [Header("회전 대상, 회전 축 [회전 대상이 null이면 자신이 회전]")]
     [SerializeField]
-    private Vector3 start;
+    private Transform target;
     [SerializeField]
-    private Vector3 end;
-    [SerializeField]
-    private float resizeTime = 1f;
+    private Vector3 axis = Vector3.forward;
 
-    private void OnEnable()
+    [Header("특정 속도로 회전")]
+    [SerializeField]
+    private float rotateSpeed = 100f;
+
+    [Header("임의의 속도로 회전")]
+    [SerializeField]
+    private bool isRandomSpeed = false;
+
+    [SerializeField]
+    private float minSpeed = 50;
+    [SerializeField]
+    private float maxSpeed = 200f;
+
+    private void Awake()
     {
-        StartCoroutine(nameof(Process));
-    }
-    private void OnDisable()
-    {
-        StopCoroutine(nameof(Process));
-    }
-    IEnumerator Process()
-    {
-        while (true)
+        if (target == null) target = transform;
+
+        if (isRandomSpeed == true)
         {
-            yield return StartCoroutine(ScaleTo(start, end));
-            yield return StartCoroutine(ScaleTo(end, start));
+            rotateSpeed = Random.Range(minSpeed, maxSpeed);
         }
     }
 
-    IEnumerator ScaleTo(Vector3 start, Vector3 end)
+    private void Update()
     {
-        float percent = 0;
-        while (percent < 1)
-        {
-            percent += Time.deltaTime / resizeTime;
-            transform.localScale = Vector3.Lerp(start, end, percent);
-            yield return null;
-        }
+        target.Rotate(axis, rotateSpeed * Time.deltaTime);
     }
 }
